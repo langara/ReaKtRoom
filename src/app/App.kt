97fede3505b86bc33ea6b84dfc8da33fad04fea6
Marek Langiewicz <marek.langiewicz@gmail.com>
2019-02-23 14:48:21 +0100

    Experiment with noh and jquery; add and display rzut_opt_4.svg

--- src/app/App.kt
+++ src/app/App.kt
@@ -5,12 +5,22 @@ import react.dom.*
 import logo.*
 import ticker.*
 
+@JsModule("src/rzut_opt_4.svg")
+external val rzut: dynamic
+
+@JsModule("jquery")
+external val jq: dynamic
+
+@JsModule("noh.js")
+external val noh: dynamic
+
 class App : RComponent<RProps, RState>() {
+
     override fun RBuilder.render() {
         div("App-header") {
             logo()
             h2 {
-                +"Welcome to React with Kotlin"
+                +"Welcome to Reacccct with Kotlin"
             }
         }
         p("App-intro") {
@@ -21,6 +31,20 @@ class App : RComponent<RProps, RState>() {
         p("App-ticker") {
             ticker()
         }
+
+        p(classes = "experiment") {}
+
+        p {
+            img(src = rzut) {}
+        }
+    }
+
+    override fun componentDidMount() {
+//        console.log(noh)
+//        console.log(jq)
+        val ex = jq(".experiment")[0]
+        console.log(ex)
+        noh.h2("ExPeRiMeNT").attachToDOM(ex)
     }
 }
 
